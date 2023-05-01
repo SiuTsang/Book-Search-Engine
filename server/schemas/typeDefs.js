064abacf6dfa82bd1128085c67ff91ca1ed4b331
siutsang <95592154+royshtsang@users.noreply.github.com>
2023-05-01 18:17:45 -0400

    updated server

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,28 +1,46 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Tech {
-    _id: ID!
-    name: String!
-  }
+    type User {
+        _id: ID
+        username: String!
+        email: String
+        savedBooks: [Book]
+        bookCount: Int
+    }
+    
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String!
+        image: String
+        link: String
+        title: String!
+    }
+    
+    type Auth {
+        token: ID!
+        user: User
+    }
+    
+    input BookInput {
+        bookId: String!
+        authors: [String]
+        description: String!
+        image: String
+        link: String
+        title: String!
+    }
+    
+    type Query {
+        self: User
+    }
+    
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookInfo: BookInput!): User
+        removeBook(bookId: ID!): User
+    }`;
 
-  type Matchup {
-    _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
-  }
-
-  type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
-  }
-
-  type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
-    createVote(_id: String!, techNum: Int!): Matchup
-  }
-`;
-
-module.exports = typeDefs;
+    module.exports = typeDefs;
